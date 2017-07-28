8dfc87bededfe719a20460db2698d7bf604caed4
picholasnaridon <paridonn1@gmail.com>
2017-07-28 15:40:12 -0400

    test

--- wp-content/themes/twentyfifteen/js/tinypass.js
+++ wp-content/themes/twentyfifteen/js/tinypass.js
@@ -21,7 +21,7 @@ tp.push(["init", function(){
         jQuery("#menu-item-4").hide(); //login
         jQuery("#menu-item-6").hide(); //signup
       } else {
-        jQuery("#menu-item-5").hide(); //logoutdsfs
+        jQuery("#menu-item-5").hide(); //logoutdsfsfsdfd
       }
     }])
 
